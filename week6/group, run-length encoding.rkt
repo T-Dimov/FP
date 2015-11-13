984dbbfa1569729336e509301d9d6ba0ad7abafd
T-Dimov <T-Dimov@users.noreply.github.com>
2015-11-13 23:45:41 +0200

    Update group, run-length encoding.rkt

--- week6/group, run-length encoding.rkt
+++ week6/group, run-length encoding.rkt
@@ -22,7 +22,8 @@
   (define (help res lst)
     (cond
       [(empty? lst) res]
-      [else (help (cons (take-while (lambda (x) (equal? x (first lst))) lst) res) (drop-while (lambda (x) (equal? x (first lst))) lst))]))
+      [else (help (cons (take-while (lambda (x) (equal? x (first lst))) lst) res)
+                  (drop-while (lambda (x) (equal? x (first lst))) lst))]))
   (reverse (help (list) input)))
 
 (define (run-length-encode input)
@@ -30,21 +31,20 @@
   (define (tail str) (substring str 1))
   (define (help str count res)
     (cond
-      [(= (string-length str) 1) (string-append res ((lambda (x) (if (not (= x 1)) (~a x) "")) count) (head str))]
+      [(= (string-length str) 1) (string-append res
+                                                ((lambda (x) (if (not (= x 1)) (~a x) "")) count)
+                                                (head str))]
       [(equal? (head str) (head (tail str))) (help (tail str) (add1 count) res)]
       [(= count 1) (help (tail str) 1 (string-append res (head str)))]
       [else (help (tail str) 1 (string-append res (~a count) (head str)))]))
-  (help input 1 "")
-  )
+  (help input 1 ""))
 
 (define (string-repeat str n)
   (define (help i res)
     (if (= i n)
         res
-        (help (+ i 1) (string-append res str)))
-    )
-  (help 0 "")
-  )
+        (help (+ i 1) (string-append res str))))
+  (help 0 ""))
 
 (define (log10 n) (/ (log n) (log 10)))
 
@@ -52,12 +52,24 @@
   (define (len str res)
     (cond
       [(zero? (string-length str)) '(0 "")]
-      [(char-numeric? (string-ref str 0)) (len (substring str 1) (+ (* res 10) (- (char->integer (string-ref str 0)) (char->integer #\0))))]
+      [(char-numeric? (string-ref str 0)) (len (substring str 1)
+                                               (+ (* res 10)
+                                                  (- (char->integer (string-ref str 0))
+                                                                (char->integer #\0))))]
       [else (cons res (string-ref str 0))]))
   (define (help str res)
-    (cond
-      [(zero? (string-length str)) res]
-      [(= (car (len str 0)) 0) (help (substring str 1) (string-append res (substring str 0 1)))]
-      [else (help (substring str (inexact->exact (+ (floor (log10 (car (len str 0)))) 2))) (string-append res (string-repeat (string (cdr (len str 0))) (car (len str 0)))))]))
+    (let ([times (car (len str 0))]
+          [letter (cdr (len str 0))])
+      (cond
+        [(zero? (string-length str)) res]
+        [(= times 0) (help (substring str 1)
+                           (string-append res
+                                          (substring str 0 1)))]
+        [else (help (substring str
+                               (inexact->exact (+ (floor (log10 times))
+                                                  2)))
+                    (string-append res
+                                   (string-repeat (string letter)
+                                                  times)))])))
   (help input "")
   )
