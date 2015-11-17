fda9e78173c0e27ca563f5bdfd5603b76b9841be
T-Dimov <T-Dimov@users.noreply.github.com>
2015-11-17 23:02:24 +0200

    Update and rename week6/group, run-length encoding.rkt to Homework-06/run-length encoding.rkt

--- Homework-06/run-length encoding.rkt
+++ Homework-06/run-length encoding.rkt
@@ -1,29 +1,5 @@
 #lang racket
 
-(define (take-while p items)
-  (define (help res lst)
-    (cond
-      [(empty? lst) res]
-      [(not (p (first lst))) res]
-      [else (help (cons (first lst) res) (rest lst))]))
-  (reverse (help (list) items)))
-
-(define (drop-while p items)
-  (define (help res)
-    (cond
-      [(empty? res) res]
-      [(not (p (first res))) res]
-      [else (help (rest res))]))
-  (help items))
-
-(define (group input)
-  (define (help res lst)
-    (cond
-      [(empty? lst) res]
-      [else (help (cons (take-while (lambda (x) (equal? x (first lst))) lst) res)
-                  (drop-while (lambda (x) (equal? x (first lst))) lst))]))
-  (reverse (help (list) input)))
-
 (define (run-length-encode input)
   (define (head str) (substring str 0 1))
   (define (tail str) (substring str 1))
@@ -70,3 +46,4 @@
                                    (string-repeat (string letter)
                                                   times)))])))
   (help input ""))
+  
